7248ee5f7807bf6d39394c07b687c8991d4661db
Adrian609 <rodriguez.adrian609@gmail.com>
2016-11-26 17:57:34 -0500

    Added code interpreted from the UML diagram
    -- UML Diagram (Report) --
    - Sections: ArrayList <Page>
    + Report (page : Page)
    + getSection(): ArrayList<Page>
    + toString(): String
    + save (filename: String) void
    + exportToExcel(fileName: String) void

--- ReportGenerator/src/edu/wcupa/cscclub/projects/martinbrower/reportgenerator/model/Report.java
+++ ReportGenerator/src/edu/wcupa/cscclub/projects/martinbrower/reportgenerator/model/Report.java
@@ -5,11 +5,52 @@
  */
 package edu.wcupa.cscclub.projects.martinbrower.reportgenerator.model;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author talk2
+ * @author Adrian
  */
-public class Report
-{
-    
+
+/*
+* -- UML Diagram (Report) --
+* - Sections: ArrayList <Page>
+* + Report (page : Page)
+* + getSection(): ArrayList<Page>
+* + toString(): String
+* + save (filename: String) void
+* + exportToExcel(fileName: String) void
+ */
+public class Report {
+
+    private ArrayList<Page> sections;
+    private ArrayList page;
+
+    // + Report (page : Page)
+    public Report(ArrayList Page) {
+        Page = page;
+    }
+
+    // + getSection(): ArrayList<Page>
+    public ArrayList<Page> getSections() {
+        return null;
+    }
+
+    // + toString(): String
+    @Override
+    public String toString() {
+        return "Report{" + page + '}';
+    }
+
+    // + save (filename: String) void
+    public void save() {
+
+    }
+
+    // + exportToExcel(fileName: String) void
+    public void exportToExcel() {
+
+    }
+;
+
 }
